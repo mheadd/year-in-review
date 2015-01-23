8d6f411848b8a9efdef41b5fad6e26a72d6efead
mheadd <mheadd@gmail.com>
2015-01-23 14:11:29 -0500

    Back out setTimeout hack

--- cities.js
+++ cities.js
@@ -1,5 +1,5 @@
 var cities = {
-	"salt-lake-city": {name: "Salt Lake City, UT", resource_id: "94701a46-ee4a-4f42-8bea-b80ce10041a8", date_field: "DATE OPENED" },
+	"omaha": {name: "Omaha, NE", resource_id: "c494826d-9b96-45c9-bfd1-0a3e31186727", date_field: "DATE OPENED" },
 	"grand-rapids" : {name: "Grand Rapids, MI", resource_id: "e741edf8-04ad-450d-bc62-6684a7a427dd", date_field: "Issued" },
 	"charlotte-county": {name: "Charlotte County, FL", resource_id: "163d8400-3ffd-4739-82c9-8c2f58ecd923", date_field: "DATE OPENED" },
 	"framingham": {name: "Framingham, MA", resource_id: "48389821-39fb-43d9-85cc-971f989a0107", date_field: "IssueDate" },
